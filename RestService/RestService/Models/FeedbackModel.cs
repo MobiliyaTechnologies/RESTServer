244a36c1cc51c21012c9d1ff84f0b38b391ce312
Prateek <Prateek@PRATEEKR-PC>
2017-02-16 15:41:32 +0530

    [#29][Adding missing files][Completed]

---
+++ RestService/RestService/Models/FeedbackModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestService.Models
+{
+    public class FeedbackModel
+    {
+        [Required]
+        public int FeedbackId { get; set; }
+        public int ClassId { get; set; }
+        public int QuestionId { get; set; }
+        public int AnswerID { get; set; }
+        [MaxLength(500)]
+        public string FeedbackDesc { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
